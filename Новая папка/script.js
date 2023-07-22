f88ddf929b28c03850060a3348ac9dccefa59738
RuslanPC <onishruslan@yandex.ru>
2023-07-22 20:31:03 +0300

    release

--- Новая папка/script.js
+++ Новая папка/script.js
@@ -7,6 +7,8 @@ buy_btn.addEventListener("click", () => {
     let email = document.getElementById('email').value;
     let text = document.getElementById('text').value;
     let time = 0
+
+
     if (document.getElementById("three-day").checked){
         time = 3
     }
@@ -19,6 +21,11 @@ buy_btn.addEventListener("click", () => {
     if (document.getElementById("none-day").checked){
         time = 100
     }
+
+    document.getElementById("user_name").style.border = "1px solid #ADD8E6"
+    document.getElementById("telegram_id").style.border = "1px solid #ADD8E6"
+    document.getElementById("email").style.border = "1px solid #ADD8E6"
+
     let data1 = {
         name:name,
         telegram_id:telegram_id,
@@ -26,25 +33,28 @@ buy_btn.addEventListener("click", () => {
         text:text,
         time:time
     }
+    let warn = 0
     if(name.length < 3){
-        alert('Ошибка в имени')
+        warn = 1
+        document.getElementById("user_name").style.border = "2px solid red"
     }
-    else if(telegram_id.length < 1){
-        alert('Ошибка в вашем телеграмми')
+    if(telegram_id.length < 1){
+        warn = 1
+        document.getElementById("telegram_id").style.border = "2px solid red"
     }
-    else if(email < 5){
-        alert('Ошибка в Email')
+    if(email < 5){
+        warn = 1
+        document.getElementById("email").style.border = "2px solid red"
     }
-    else{
+
+    if (warn != 1){
         if(time != 0){
             alert('Спасибо. С вами свяжутся')
-            tg.sendData(JSON.stringify(data));
+            tg.sendData(JSON.stringify(data1));
             setInterval( () => tg.close(),3000);
         }else{
             alert('Не выбран срок готовности')
         }
-
     }
-
 });
 
--- Новая папка/telegram_shop.py
+++ Новая папка/telegram_shop.py
@@ -2,6 +2,7 @@ import types
 from aiogram.types.web_app_info import WebAppInfo
 from aiogram import *
 import json
+import sqlite3
 
 bot = Bot("6184823844:AAE7JvBRB4shgFkLd2353I9ihWf4Ggtkr74")
 dp = Dispatcher(bot)
@@ -9,7 +10,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message:types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Форма заказа',web_app=WebAppInfo(url='https://ober1.st8.ru/tg/telegram.html')))
+    markup.add(types.KeyboardButton('Форма заказа',web_app=WebAppInfo(url='https://ober1.st8.ru/tg/new/telegram.html')))
     await message.answer('Привет', reply_markup=markup)
 
 @dp.message_handler(content_types=['web_app_data'])
@@ -21,7 +22,16 @@ async def web_app(message:types.Message):
     tg = res['telegram_id']
     time = res['time']
     print(name,email,tg,text, time)
+    string = f'{name},{email},{tg},{time},{text}\n'
+    with open('req.txt', 'a', encoding='utf-8')as file:
+        file.write(string)
 
-
-
+@dp.message_handler(commands=['test'])
+async def test(message:types.Message):
+    res = []
+    with open('req.txt', 'r', encoding='utf-8')as file1:
+        for i in file1.readlines():
+            res.append(i)
+    for i in res:
+        await message.answer(i)
 executor.start_polling(dp)
